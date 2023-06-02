ff75262ff4f0dac28d67937d93f4d03fd13b3b1b
Dhrubo Paul <dhrubo.paul@pax.us>
2023-06-02 17:44:03 -0400

    ShowItemFragment crashes if messageList is not present (HZ app)

--- app/src/main/java/com/paxus/pay/poslinkui/demo/entry/poslink/ShowItemFragment.java
+++ app/src/main/java/com/paxus/pay/poslinkui/demo/entry/poslink/ShowItemFragment.java
@@ -12,11 +12,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.pax.us.pay.ui.constant.entry.EntryExtraData;
-import com.pax.us.pay.ui.constant.entry.EntryRequest;
 import com.pax.us.pay.ui.constant.entry.PoslinkEntry;
 import com.paxus.pay.poslinkui.demo.R;
 import com.paxus.pay.poslinkui.demo.entry.BaseEntryFragment;
-import com.paxus.pay.poslinkui.demo.utils.EntryRequestUtils;
 import com.paxus.pay.poslinkui.demo.utils.Logger;
 
 import org.json.JSONArray;
@@ -39,13 +37,8 @@ public class ShowItemFragment extends BaseEntryFragment {
     private String taxLine;
     private String totalLine;
     private String currencySymbol;
-
-    RecyclerView recyclerViewShowItem;
-    TextView tvTotalLineShowItem;
-    TextView tvTaxLineShowItem;
-    LinearLayout titleLayout;
-
     private List<ItemDetailWrapper> itemWrapperList = new ArrayList<>();
+
     private RecyclerView.Adapter itemListAdapter;
 
     @Override
@@ -61,21 +54,19 @@ public class ShowItemFragment extends BaseEntryFragment {
         currencySymbol = bundle.getString(EntryExtraData.PARAM_CURRENCY_SYMBOL);
 
         itemWrapperList = parseItemList(bundle.getString(EntryExtraData.PARAM_MESSAGE_LIST));
-
     }
 
     @Override
     protected void loadView(View rootView) {
-        titleLayout = rootView.findViewById(R.id.tv_title_show_item);
-        tvTotalLineShowItem = rootView.findViewById(R.id.tv_total_line_show_item);
-        tvTaxLineShowItem = rootView.findViewById(R.id.tv_tax_line_show_item);
-        recyclerViewShowItem = rootView.findViewById(R.id.recycler_View_show_item);
+        LinearLayout titleLayout = rootView.findViewById(R.id.tv_title_show_item);
+        TextView tvTotalLineShowItem = rootView.findViewById(R.id.tv_total_line_show_item);
+        TextView tvTaxLineShowItem = rootView.findViewById(R.id.tv_tax_line_show_item);
+        RecyclerView recyclerViewShowItem = rootView.findViewById(R.id.recycler_View_show_item);
 
-
-        if(title == null || title.isEmpty()){
+        if (title == null || title.isEmpty()) {
             titleLayout.setVisibility(View.GONE);
-        }else {
-            for(TextView textView: TextShowingUtils.getTextViewList(requireContext(),title)){
+        } else {
+            for (TextView textView : TextShowingUtils.getTextViewList(requireContext(), title)) {
                 titleLayout.addView(textView);
             }
             titleLayout.setVisibility(View.VISIBLE);
@@ -87,7 +78,7 @@ public class ShowItemFragment extends BaseEntryFragment {
         recyclerViewShowItem.setLayoutManager(linearLayoutManager);
         recyclerViewShowItem.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
 
-        if(itemWrapperList != null) {
+        if (itemWrapperList != null) {
             itemListAdapter = new ItemListAdapter(requireContext(), itemWrapperList, currencySymbol);
             recyclerViewShowItem.setAdapter(itemListAdapter);
         }
@@ -100,7 +91,7 @@ public class ShowItemFragment extends BaseEntryFragment {
         sendNext(null);
     }
 
-    private List<ItemDetailWrapper> parseItemList(String jsonString){
+    private List<ItemDetailWrapper> parseItemList(String jsonString) {
         try {
             List<ItemDetailWrapper> list = new ArrayList<>();
             JSONArray jsonArray = new JSONArray(jsonString);
@@ -122,12 +113,12 @@ public class ShowItemFragment extends BaseEntryFragment {
                         msgObj.optString("quantity"),
                         msgObj.optString("productImgUri"),
                         msgObj.optString("productImgDesc")
-                        ));
+                ));
                 list.add(item);
             }
 
             return list;
-        } catch (JSONException e) {
+        } catch (JSONException | NullPointerException e) {
             Logger.e(e);
         }
 
